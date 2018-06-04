278de86a6b30f55f4d60efc3e4392417e4447fb9
sammarcus <sam.marcus@me.com>
2018-06-04 11:27:50 -0400

    solution

--- index.rb
+++ index.rb
@@ -6,28 +6,34 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+  order.each do |want|
+    found = ingredients.find do |arr|
+      arr.find {|have| want == have}
+    end
+    return false if !found
+  end
+  true
 end
 
 # example city_info hash
 cities = {
-  :new_york => { 
+  :new_york => {
     :mayor => "Bill DeBlasio",
     :population => 8337000,
     :website => "http://www.nyc.gov"
   },
-  :los_angeles => { 
+  :los_angeles => {
     :mayor => "Eric Garcetti",
     :population => 3884307,
     :website => "http://www.lacity.org"
   },
-  :miami => { 
+  :miami => {
     :mayor => "Tomás Regalado",
     :population => 419777,
     :website => "http://www.miamigov.com"
   },
-  :chicago => { 
+  :chicago => {
     :mayor => "Rahm Emanuel",
     :population => 2695598,
     :website => "http://www.cityofchicago.org/"
@@ -35,5 +41,5 @@ cities = {
 }
 
 def city_populations(cities)
-  # your code here!
+  cities.values.sum {|city| city[:population]}
 end
